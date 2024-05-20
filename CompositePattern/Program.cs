7756d5c50551923460bd8884a817a3e1c38106b8
George Vames <george.vames@morecommerce.com>
2024-05-20 12:53:20 +0300

    Adaugat fisierul .gitignore si eliminat fisiere ignorate din cache-ul Git

--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CompositePattern
 {
@@ -10,6 +7,63 @@ namespace CompositePattern
     {
         static void Main(string[] args)
         {
+            // Crearea componentelor
+            Leaf leaf1 = new Leaf();
+            Leaf leaf2 = new Leaf();
+            Leaf leaf3 = new Leaf();
+
+            // Crearea componentei compuse
+            Composite composite = new Composite();
+            composite.Add(leaf1);
+            composite.Add(leaf2);
+
+            // Adăugarea unui alt composite într-un composite
+            Composite subComposite = new Composite();
+            subComposite.Add(leaf3);
+            composite.Add(subComposite);
+
+            // Apelarea operației pe componenta compusă
+            composite.Operation(0); // Nivelul de indentare începe de la 0
+        }
+
+        // Interfață comună pentru toate componente
+        public interface IComponent
+        {
+            void Operation(int indentLevel);
+        }
+
+        // Implementare a componentelor individuale
+        public class Leaf : IComponent
+        {
+            public void Operation(int indentLevel)
+            {
+                Console.WriteLine(new String('\t', indentLevel) + "Operation executed in Leaf");
+            }
+        }
+
+        // Implementare a componentei compuse
+        public class Composite : IComponent
+        {
+            private List<IComponent> children = new List<IComponent>();
+
+            public void Add(IComponent component)
+            {
+                children.Add(component);
+            }
+
+            public void Remove(IComponent component)
+            {
+                children.Remove(component);
+            }
+
+            public void Operation(int indentLevel)
+            {
+                Console.WriteLine(new String('\t', indentLevel) + "Operation executed in Composite");
+                foreach (var component in children)
+                {
+                    component.Operation(indentLevel + 1); // Crește nivelul de indentare pentru copii
+                }
+            }
         }
     }
 }
